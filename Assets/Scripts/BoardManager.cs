4c8d4d81095c70d07c8c14fb99aa058885d9c65d
Eric Ni <en1116@nyu.edu>
2021-03-14 19:16:08 -0400

    Player and Sword Incorporation
    Moving files out of folder due to github access issues. Also, added the sword assets package and added the ability for players to move around the board and to hold a positioned sword.

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour
+{
+
+    public GameObject Checkerboard;
+    public GameObject player1;
+    public GameObject SpawnPoint;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //player = GameObject.Find("White King(Clone)");
+        SpawnPoint = GameObject.Find("SpawnPoint");
+        
+
+        //https://assetstore.unity.com/packages/3d/chess-pieces-and-board-95641    
+        //spawns the player onto the player on a spawnpoint, with both taken from the above free assets package. 
+
+        //Instantiate(player1, SpawnPoint.transform.position, player1.transform.rotation);
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+        
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float followSpeed;
+    
+    public Camera cam;
+
+    public Rigidbody rigidbody;
+
+ 
+    
+    
+    //public BoardManager manager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        followSpeed = 5f;
+        rigidbody = GetComponent<Rigidbody>();
+        //manager = gameObject.AddComponent<BoardManager>();
+        cam = Camera.main;
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        //This helps control the player's movement. 
+        //https://gamedevacademy.org/unity-3d-first-and-third-person-view-tutorial //[
+
+        
+        
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+
+        
+
+
+
+
+        //Code from Nimso Ny's tutorial on moving the player relative to the camera. https://www.youtube.com/watch?v=ORD7gsuLivE 
+        Vector3 camForward = cam.transform.forward;
+        Vector3 camRight = cam.transform.right;
+
+        camForward.y = 0;
+        camRight.y = 0;
+        camForward = camForward.normalized;
+        camRight = camRight.normalized;
+
+        //using transform position does not have as good of collisions, so I am using the movement from Rigidbody instead. 
+        //transform.position += ((camForward * z + camRight * x) * Time.deltaTime * followSpeed);
+        rigidbody.AddForce((camRight*x + camForward*z)* followSpeed );
+
+        //]
+
+
+
+
+    }
+}
